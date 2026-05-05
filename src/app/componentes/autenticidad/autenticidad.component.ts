2f15bc3bc973b49ef44598715dc8ffb361ee8024
JaviDomingo <javidomingo@digitechfp.com>
2026-05-05 19:56:51 +0200

    Pagina de autenticidad completa y funcional

--- src/app/componentes/autenticidad/autenticidad.component.ts
+++ src/app/componentes/autenticidad/autenticidad.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-autenticidad',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './autenticidad.component.html',
   styleUrl: './autenticidad.component.css'
 })
