2f303d245f5c8a6a6c6f1272f373a20f627f9360
JaviDomingo <javidomingo@digitechfp.com>
2026-05-15 19:01:16 +0200

    Creacion productointerface y confi appconfig https with fetch

---
+++ src/app/common/producto-interface.ts
@@ -0,0 +1,17 @@
+export type Root = ProductoInterface[]
+
+  export interface ProductoInterface {
+  id: string
+  name: string
+  brand: string
+  price: number
+  images: string[]
+  material: string
+  diametro: string
+  calibre: string
+  reserva_marcha: string
+  ano: number
+  categoria: string
+  descripcion: string
+
+}
